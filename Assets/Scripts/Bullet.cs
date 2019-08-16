f215b38b3cb066a045a30ec0f33bc6a08383c1ba
sonic1573 <sonic157346@gmail.com>
2019-08-16 20:30:38 +0900

    adding bullets & weapon base

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public static float bulletDamage = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //프레임마다 오브젝트를 로컬좌표상에서 앞으로 1의 힘만큼 날아가라
+        transform.Translate(Vector3.forward * 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")     //충돌상대 필터링
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+}
