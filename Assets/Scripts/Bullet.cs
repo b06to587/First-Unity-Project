8c697aa2c25bf8e5aba734ecd23229847e8cd680
sonic1573 <sonic157346@gmail.com>
2019-08-17 01:31:37 +0900

    무기변경

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,15 +9,14 @@ public class Bullet : MonoBehaviour
     public static float weaponMachineGunDamage = 1f;
     public static float weaponMachineGunDistance = 0.05f;
 
+    public static float weaponDamage = weaponBitGunDamage;
+    public static float weaponDistance = weaponBitGunDistance;
+
     void Start()
     {
         
     }
-
-    public static float weaponDamage = weaponBitGunDamage;
-    public static float weaponDistance = weaponBitGunDistance;
-
-
+    
     void Update()
     {
         //프레임마다 오브젝트를 로컬좌표상에서 앞으로 1의 힘만큼 날아가라
