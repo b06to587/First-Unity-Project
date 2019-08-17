2111abf5f994a8bc1007cc80a63265911df3115d
KimBamYa <recon7347@gmail.com>
2019-08-18 01:08:48 +0900

    월무버랑 크리에이터 리펙토링 커밋

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,69 +11,46 @@ public int SpawnerToidentitynumber=0;
 public List<GameObject> Wall = new List<GameObject>(); 
 public Quaternion ori = Quaternion.Euler(new Vector3(0,0,0));
 public Quaternion plus = Quaternion.Euler(new Vector3(0,72,0));
-public int[] pentagon = new int[] {108,36,-36,-108,0};
+public int[] pentagon = new int[5] {108,36,-36,-108,180};
+public int wallMoveSpeed=100;
+
+public int WallPosX =10; //월 생성 위치
     
     // Start is called before the first frame update
     void Start(){
+        creatPentagon();
+       
+       
+        
   
-        Wall.Add(Instantiate(prefabWall));
-        identityWall.identitnumbe+=1;
-        Wall[0].transform.parent=center.transform;
-        center.transform.rotation=Quaternion.Euler(new Vector3(0,72*0,0));
-        Wall[0].transform.position=new Vector3(10,0,0);
-       
-
-        Wall.Add(Instantiate(prefabWall));
-        identityWall.identitnumbe+=1;
-        Wall[1].transform.parent=center.transform;
-        center.transform.rotation=Quaternion.Euler(new Vector3(0,72*1,0));
-        Wall[1].transform.position=new Vector3(10,0,0);
-     
-
-        Wall.Add(Instantiate(prefabWall));
-        identityWall.identitnumbe+=1;
-        Wall[2].transform.parent=center.transform;
-        center.transform.rotation=Quaternion.Euler(new Vector3(0,72*2,0));
-        Wall[2].transform.position=new Vector3(10,0,0);
-      
-
-        Wall.Add(Instantiate(prefabWall));
-        identityWall.identitnumbe+=1;
-        Wall[3].transform.parent=center.transform;
-        center.transform.rotation=Quaternion.Euler(new Vector3(0,72*3,0));
-        Wall[3].transform.position=new Vector3(10,0,0);
-      
-
-        Wall.Add(Instantiate(prefabWall));
-        identityWall.identitnumbe+=1;
-        Wall[4].transform.parent=center.transform;
-        center.transform.rotation=Quaternion.Euler(new Vector3(0,72*4,0));
-        Wall[4].transform.position=new Vector3(10,0,0);
-
-        Wall[0].transform.parent=null;
-        Wall[1].transform.parent=null;
-        Wall[2].transform.parent=null;
-        Wall[3].transform.parent=null;
-        Wall[4].transform.parent=null;
-
-        Wall[0].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[0],0));
-        Wall[1].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[1],0));
-        Wall[2].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[2],0));
-        Wall[3].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[3],0));
-        Wall[4].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[4],0));
-
-       
     }
 
     // Update is called once per frame
     void Update()
     {   
-     
-  
-   
+ WallMover();
         
 
     }
+    public void creatPentagon(){
+        for(int i=0; i<=4; i++){
+            Wall.Add(Instantiate(prefabWall));
+            identityWall.identitnumbe+=1;
+            Wall[i].transform.parent=center.transform;
+            center.transform.rotation=Quaternion.Euler(new Vector3(0,72*i,0));
+            Wall[i].transform.position=new Vector3(WallPosX,0,0);
 
+        }//end of for(0)
+        for(int j = 0; j<=4; j++){
+            Wall[j].transform.parent=null;
+            Wall[j].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[j],0));
+        }//end of for(1);
+    }//end of createPentaagon()
+    public void WallMover(){
+        for(int i = 0; i<=4;i++){
+       Vector3 velocity = Wall[i].transform.position-center.transform.position;
+       Wall[i].transform.position-= velocity/wallMoveSpeed;
+        }
+    }
   
     }
