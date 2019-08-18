9eb8fe5417cf574b0dd61da7862ab5a641ba1e7b
KKBJ <b06to587@gmail.com>
2019-08-18 22:16:09 +0900

    벽을 줄어들게한다

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,37 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-public GameObject center;
-public GameObject prefabWall;
-public Wall identityWall;
-public int SpawnerToidentitynumber=0;
-public List<GameObject> Wall = new List<GameObject>(); 
-public Quaternion ori = Quaternion.Euler(new Vector3(0,0,0));
-public Quaternion plus = Quaternion.Euler(new Vector3(0,72,0));
-public int[] pentagon = new int[5] {108,36,-36,-108,180};
-public int wallMoveSpeed=100;
+    public GameObject center;
+    public GameObject prefabWall;
+    public Wall identityWall;
+    public int SpawnerToidentitynumber=0;
+    public List<GameObject> Wall = new List<GameObject>(); 
+    public Quaternion ori = Quaternion.Euler(new Vector3(0,0,0));
+    public Quaternion plus = Quaternion.Euler(new Vector3(0,72,0));
+    public int[] pentagon = new int[5] {108,36,-36,-108,180};
+    public int wallMoveSpeed=1000;
 
-public int WallPosX =10; //월 생성 위치
+    public int WallPosX =10; //월 생성 위치
     
     // Start is called before the first frame update
     void Start(){
         creatPentagon();
-       
-       
-        
-  
     }
 
     // Update is called once per frame
     void Update()
     {   
- WallMover();
-        
-
+        WallMover();
+        WallShrink();
     }
+
     public void creatPentagon(){
         for(int i=0; i<=4; i++){
-            Wall.Add(Instantiate(prefabWall));
+            Wall.Add(Instantiate(prefabWall) as GameObject);
             identityWall.identitnumbe+=1;
             Wall[i].transform.parent=center.transform;
             center.transform.rotation=Quaternion.Euler(new Vector3(0,72*i,0));
@@ -46,11 +42,20 @@ public int WallPosX =10; //월 생성 위치
             Wall[j].transform.rotation=Quaternion.Euler(new Vector3(0,pentagon[j],0));
         }//end of for(1);
     }//end of createPentaagon()
+
     public void WallMover(){
         for(int i = 0; i<=4;i++){
-       Vector3 velocity = Wall[i].transform.position-center.transform.position;
-       Wall[i].transform.position-= velocity/wallMoveSpeed;
+            Vector3 velocity = Wall[i].transform.position-center.transform.position;
+            Wall[i].transform.position-= velocity/wallMoveSpeed;
+        }
+    }
+
+    private void WallShrink(){
+        float rate =  1-((float)1/wallMoveSpeed);
+        for (int i = 0; i < 5; i++)
+        {
+            Wall[i].transform.localScale *= rate;
         }
     }
   
-    }
+}
