4569eab8fcf750d12f47f2745bf8553efa87c14c
sonic1573 <sonic157346@gmail.com>
2019-08-31 17:45:33 +0900

    게임오버, 일시정지, UI틀 완성 (문제: pause시 스폰은 멈추는데 벽 포지션은 계속 줄어듬)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,39 +4,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    public static bool gameOver = false;
     private Spawner spawner;
     public GameObject PauseMenu;
-    public GameObject Button_Resume;
-    public GameObject Button_End;
-    //public Player player;
-    // Start is called before the first frame update
-
 
     void Start()
     {
-        //Time.timeScale = 0
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Escape)){
             GamePause();
         }
     }
-    private void GamePause()
+    
+    public void GameStart()
+    {
+        SceneManager.LoadScene("main");
+        Time.timeScale = 1;
+    }
+
+    public void GamePause()
     {
         PauseMenu.SetActive(true);    
         Time.timeScale = 0;
     }
-    public void GameResume(){
+
+    public void GameContinue()
+    {
         PauseMenu.SetActive(false);    
         Time.timeScale = 1;
     }
+
     public void GameEnd()
     {
         SceneManager.LoadScene("Start");
     }
+
+    
 
 }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,16 +7,37 @@ public class Menu : MonoBehaviour
 {
     public GameObject BG;
     public GameObject MainMenu;
-    public GameObject Button_Start;
-    public GameObject Button_Option;
+    public GameObject PauseMenu;
+    public GameObject GameOverMenu;
 
+    public bool gameOver = false;
 
-    public void GameStart(int num)
+    void Start()
     {
-       SceneManager.LoadScene(num);
+        //Time.timeScale = 0;
     }
 
+    void Update()
+    {
+        if(gameOver == true)
+        {
+            GameOver();
+            gameOver = false;
+        }
+    }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")     
+        {
+            gameOver = true;
+        }
+    }
 
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        GameOverMenu.SetActive(true);
+    }
     
 }
