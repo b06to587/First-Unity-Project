14f659cac9c7da62fdd9b789df5d7e8ddc3d4661
KKBJ <b06to587@gmail.com>
2019-09-07 19:56:05 +0900

    사운드 작업

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 //오디오를 컨트롤하는 스크립트
 public class AudioManager : MonoBehaviour
 {
-   public enum AudioChannel {Master,Sfx,Music};  // 오디오를 오디오채널이라는 이름으로 열거
    public float masterVolumePercent {get;private set;} // get접근자는 public 으로 받을수있지만, set 접근자는 private으로 제한된 접근
-   public float sfxVolumePercent {get;private set;}
-   public float musicVolumePercent {get;private set;}
 
     AudioSource sfx2DSource;    //  오디오2d 사운드 이팩트 소스 
 
@@ -37,29 +34,17 @@ public class AudioManager : MonoBehaviour
             newSfx2DSource.transform.parent = transform;
 
             masterVolumePercent =  PlayerPrefs.GetFloat("master vol",1);     //볼륨 값을 가져옴 없으면 기본값 1
-            sfxVolumePercent =  PlayerPrefs.GetFloat("sfx vol",1);
-            musicVolumePercent =  PlayerPrefs.GetFloat("music vol",1);
 
         }
         
     }
-    public void SetVolume(float volumePercent, AudioChannel channel){   //볼륨을 설정하는 매소드 
-        switch(channel){                                                //스위치 구문 채널에따라 볼륨 퍼센트 설정
-            case AudioChannel.Master:
-                masterVolumePercent = volumePercent;
-                break;
-            case AudioChannel.Sfx:
-                sfxVolumePercent = volumePercent;
-                break;
-            case AudioChannel.Music:
-                musicVolumePercent = volumePercent;
-                break;
-        }
-        musicSources.volume = musicVolumePercent * masterVolumePercent;
+    public void SetVolume(float volumePercent){   //볼륨을 설정하는 매소드 
+ 
+        masterVolumePercent = volumePercent;
+ 
+        musicSources.volume = masterVolumePercent;
 
         PlayerPrefs.SetFloat("master vol",masterVolumePercent);         // 마스터 볼륨 퍼센트로 결정
-        PlayerPrefs.SetFloat("sfx vol",sfxVolumePercent);
-        PlayerPrefs.SetFloat("music vol",musicVolumePercent);
         PlayerPrefs.Save();                                             // 볼륨값을 저장
     }
     public void PlayMusic(AudioClip clip, float fadeDuration = 1){  // 음악을 플래이하는 메소드
@@ -67,7 +52,7 @@ public class AudioManager : MonoBehaviour
         musicSources.Play();                                //플레이
     }  
     public void PlaySound2D(string soundName){                  // 사운드이팩트를 사운드이름으로 받아서 플레이하는 메소드
-        sfx2DSource.PlayOneShot(library.GetClipFromName(soundName),sfxVolumePercent * masterVolumePercent);     // 한번재생
+        sfx2DSource.PlayOneShot(library.GetClipFromName(soundName), masterVolumePercent);     // 한번재생
     }
 
 }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,31 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Slider volumeSliders;      
     public GameObject mainMenuHolder;               //메인메뉴를 가지고있는 오브젝트
     public GameObject optionsMenuHolder;    
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeSliders.value = AudioManager.instance.masterVolumePercent;     
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-     public void GameStart()
+    public void GameStart()
     {
         SceneManager.LoadScene("main");
         Time.timeScale = 1;
     }
 
-    public void OptionsMenu(){              //옵션메뉴를 눌렀을때
+    public void OptionsMenu()
+    {              //옵션메뉴를 눌렀을때
         mainMenuHolder.SetActive(false);       //기본메뉴는 inactive
         optionsMenuHolder.SetActive(true);      // 옵션메뉴 activeㅉ
+    }
+
+    public void SetMasterVolume(float value){           //마스터볼륨 설정하는 함수
+        AudioManager.instance.SetVolume(value);
     }
 
 }
